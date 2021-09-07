392fc588168d2ef836ffea9e6ce028048a8fe7f0
Bogdanlol <luca.bogdan95@gmail.com>
2021-09-07 13:41:50 +0300

    chore(models): add connectors

---
+++ models/connector.go
@@ -0,0 +1,27 @@
+package models
+
+import "github.com/jinzhu/gorm"
+
+type ConnectorEntity struct {
+	gorm.Model
+	Name           string `json:"name" gorm:"not null"`
+	ConnectorClass string `json:"connector.class" gorm:"not null"`
+	TasksMax       uint16 `json:"tasks.max" gorm:"not null"`
+	KeyConverter   string `json:"key.converter,omitempty" gorm:"null"`
+	ValueConverter string `json:"value.converter,omitempty" gorm:"null"`
+	Topics         string `json:"topics" gorm:"not null"`
+	File           string `json:"file,omitempty" gorm:"null"`
+	Type           string `gorm:"null"`
+	Status         string `json:"status" gorm:"null"`
+}
+
+func (ConnectorEntity) GetTableName() string {
+	return "connector"
+}
+
+type ConnectorModel interface {
+	GetById(uint64) (*ConnectorEntity, error)
+	Create(*ConnectorEntity) (*ConnectorEntity, error)
+	Update(*ConnectorEntity) (*ConnectorEntity, error)
+	Delete(uid uint32) error
+}
