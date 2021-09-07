cf7dd952eaf82fe5fc6d4ac7daabbf19df9baea0
Bogdanlol <luca.bogdan95@gmail.com>
2021-09-07 13:42:13 +0300

    feat(router): add cors router

---
+++ routes/router.go
@@ -0,0 +1,33 @@
+package routes
+
+import (
+	"streamingservice/controllers"
+
+	"github.com/gin-gonic/gin"
+)
+
+func CORS() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:8080")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "*")
+
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, HEAD, POST, PUT, DELETE, OPTIONS, PATCH")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
+}
+
+func New() *gin.Engine {
+
+	router := gin.Default()
+	// router.Use(cors.Default())
+	router.Use(CORS())
+	router.GET("/connectors", controllers.FindConnectors)
+	return router
+}
