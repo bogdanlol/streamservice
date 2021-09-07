e691691437d674afa15887c696de0426a1ca2f38
Bogdanlol <luca.bogdan95@gmail.com>
2021-09-07 13:41:28 +0300

    chore(gorm): add automigrate

--- db/db.go
+++ db/db.go
@@ -2,13 +2,14 @@ package db
 
 import (
 	"fmt"
+	"streamingservice/models"
 
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
 
 func New() *gorm.DB {
-	db, err := gorm.Open("mysql", "user:pass@/streamservice?charset=utf8&parseTime=True&loc=Local")
+	db, err := gorm.Open("mysql", "root:pass@/streamservice?charset=utf8&parseTime=True&loc=Local")
 	if err != nil {
 		fmt.Println("eror during connection to the database :", err)
 	}
@@ -17,10 +18,16 @@ func New() *gorm.DB {
 	return db
 }
 func TestDB() *gorm.DB {
-	db, err := gorm.Open("mysql", "user:pass@/streamservice?charset=utf8&parseTime=True&loc=Local")
+	db, err := gorm.Open("mysql", "root:pass@/streamservice?charset=utf8&parseTime=True&loc=Local")
 	if err != nil {
 		fmt.Println("eror during connection to the database :", err)
 	}
 	db.LogMode(true)
 	return db
+}
+
+func AutoMigrate(db *gorm.DB) {
+	db.AutoMigrate(
+		&models.ConnectorEntity{},
+	)
 }
