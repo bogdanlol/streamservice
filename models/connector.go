4e300882d145ca8f86c9cb2a97c536feaf65d304
Bogdanlol <luca.bogdan95@gmail.com>
2021-09-08 15:44:19 +0300

    chore(models): rename types

--- models/connector.go
+++ models/connector.go
@@ -5,13 +5,13 @@ import "github.com/jinzhu/gorm"
 type ConnectorEntity struct {
 	gorm.Model
 	Name           string `json:"name" gorm:"not null"`
-	ConnectorClass string `json:"connectorClass" gorm:"not null"`
-	TasksMax       uint16 `json:"tasksMax" gorm:"not null"`
-	KeyConverter   string `json:"keyConverter,omitempty" gorm:"null"`
-	ValueConverter string `json:"valueConverter,omitempty" gorm:"null"`
+	ConnectorClass string `json:"connector.class" gorm:"not null"`
+	TasksMax       uint16 `json:"tasks.max" gorm:"not null"`
+	KeyConverter   string `json:"key.converter,omitempty" gorm:"null"`
+	ValueConverter string `json:"value.converter,omitempty" gorm:"null"`
 	Topics         string `json:"topics" gorm:"not null"`
 	File           string `json:"file,omitempty" gorm:"null"`
-	Type           string `gorm:"null"`
+	Type           string `json:"type" gorm:"null"`
 	Status         string `json:"status" gorm:"null"`
 }
 
